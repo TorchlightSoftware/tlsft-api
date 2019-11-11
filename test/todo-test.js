9f0e32fa1acc9ceaea93a5b203e69c20560696ee
bitmage <q@bitmage.net>
2019-11-11 15:30:39 -0700

    wrote failing test for todo add

--- test/todo-test.js
+++ test/todo-test.js
@@ -12,13 +12,33 @@ const axios = require('axios').create({
 })
 
 // When I send a get request to /todos
-it('should get /todos', async () => {
+it('should list todos', async () => {
   const response = await axios.get('/todos')
   //console.log(response.data)
   expect(response.status).to.equal(200)
 
   // Then I should receive an array
   expect(response.data).to.eql([])
+})
+
+const newTodo = {
+  assigned: 'Brandon',
+  message: 'get milk'
+}
+it('should create a todo', async () => {
+  const response = await axios.post('/todos', newTodo)
+  //console.log(response.data)
+  expect(response.status).to.equal(201)
+})
+
+// When I send a get request to /todos
+it('should list todos', async () => {
+  const response = await axios.get('/todos')
+  //console.log(response.data)
+  expect(response.status).to.equal(200)
+
+  // Then I should see the item I just created
+  expect(response.data).to.eql([newTodo])
 })
 
 after('close api', async() => api.close())
