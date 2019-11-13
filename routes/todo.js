5985c300654a07ba1e0395b9e5c113358e3c79fc
Martin Patino <mpatino117@gmail.com>
2019-11-13 15:41:30 -0700

    helped milan starting

--- routes/todo.js
+++ routes/todo.js
@@ -12,8 +12,8 @@ module.exports = async function routes(fastify, options) {
   fastify.post('/todos', async (req, reply) => {
     // save to MongoDB
     const {Todo} = fastify.mongo.db.models
-    await Todo.create(req.body)
-    reply.code(201).send()
+    let createdTodo = await Todo.create(req.body)
+    reply.code(201).send(createdTodo)
   })
 
   // Get a single todo
