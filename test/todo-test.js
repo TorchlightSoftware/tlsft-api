bd04674d5f1dce85c68dfddc316217fd225113f8
MilanPrpa <milanprpa25@gmail.com>
2019-11-15 12:15:47 -0700

    Pass listed api tests

--- test/todo-test.js
+++ test/todo-test.js
@@ -1,19 +1,20 @@
 'use strict'
 /* eslint-disable no-unused-expressions */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
 const boiler = require('./helpers/boiler')
 
 // some test data
 const newTodo = {
+
   assigned: 'Brandon',
   message: 'get milk'
 }
 
-boiler('todos', ['clearDataBeforeAndAfter'], function() {
+boiler('todos', ['clearDataBeforeAndAfter'], function () {
 
   // When I send a get request to /todos
-  it('should list empty todos', async function() {
+  it('should list empty todos', async function () {
     const response = await this.api.get('/todos')
     //console.log(response.data)
     expect(response.status).to.equal(200)
@@ -23,16 +24,16 @@ boiler('todos', ['clearDataBeforeAndAfter'], function() {
   })
 
   let newTodoId
-  it('should create a todo', async function() {
+  it('should create a todo', async function () {
     const response = await this.api.post('/todos', newTodo)
     //console.log(response.data)
     expect(response.status).to.equal(201)
-    expect(response.data.id, 'id').to.exist
-    newTodoId = response.data.id
+    expect(response.data._id, '_id').to.exist
+    newTodoId = response.data._id
   })
 
   // When I send a get request to /todos
-  it('should list newly created todo', async function() {
+  it('should list newly created todo', async function () {
     const response = await this.api.get('/todos')
     expect(response.status).to.equal(200)
 
@@ -41,7 +42,7 @@ boiler('todos', ['clearDataBeforeAndAfter'], function() {
   })
 
   // When I request the specific Todo
-  it('should get newly created todo', async function() {
+  it('should get newly created todo', async function () {
     const response = await this.api.get(`/todos/${newTodoId}`)
     expect(response.status).to.equal(200)
 
@@ -50,9 +51,9 @@ boiler('todos', ['clearDataBeforeAndAfter'], function() {
   })
 
   // When I update a Todo
-  it('should get newly created todo', async function() {
+  it('should get newly created todo', async function () {
     let response = await this.api.patch(`/todos/${newTodoId}`,
-      {assigned: 'Tommy'}
+      { assigned: 'Tommy' }
     )
     expect(response.status).to.equal(200)
 
